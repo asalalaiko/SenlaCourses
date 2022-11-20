76bf5d61d512db228d25856c89349530550ffeff
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-11-20 18:49:53 +0300

    next part

---
+++ force-app/main/default/lwc/formsFromPropertyes/formsFromPropertyes.js
@@ -0,0 +1,82 @@
+import { LightningElement, api, track } from 'lwc';
+import PROPERTY_OBJECT from '@salesforce/schema/Property__c';
+import FIELD_ADDRESS from '@salesforce/schema/Property__c.Address__c';
+import FIELD_SELLING_PRICE from '@salesforce/schema/Property__c.Selling_price__c';
+import FIELD_RENT_PRICE from '@salesforce/schema/Property__c.Rent_price__c'
+
+
+export default class FieldsFromPropertyes extends LightningElement {
+    @api recordId;
+    @api recordTypeId;
+    @track elements = [{ id: 0 }];
+    isDeleteButtonDisabled = true;
+    isAddButtonDisabled = false;
+
+    addRow() {
+        if(this.elements.length < 3){
+            let newElement = {id: this.elements.length};
+            this.elements.push(newElement);
+        }
+        if (this.elements.length == 2) {
+            this.isDeleteButtonDisabled = false;
+        }
+        if (this.elements.length == 3) {
+            this.isAddButtonDisabled = true;
+        }
+    }
+
+    deleteRow(){
+        if (this.elements.length > 1){
+            this.elements.pop();
+        }
+        if (this.elements.length == 1) {
+            this.isDeleteButtonDisabled = true;
+        }
+        if (this.elements.length == 2) {
+            this.isAddButtonDisabled = false;
+        }
+    }
+
+    handleCancel() {
+        this.elements = [{ id: 0 }];
+        this.dispatchEvent(new CustomEvent("recordcreation"));
+    }
+
+    handleSubmit() {
+        let isValidity = true;
+        this.template.querySelectorAll('lightning-input-field').forEach(element => {
+            isValidity = isValidity && element.reportValidity();
+        });
+        if (isValidity) {
+            try {
+                this.template.querySelectorAll('lightning-record-edit-form').forEach(element => {
+                    element.submit();
+                });
+
+                this.displaySuccess();
+                this.handleCancel();
+            } catch(error) {
+                this.displayError(error);
+            }
+        } 
+    }
+
+    displaySuccess() {
+        const toastEvent = new ShowToastEvent({
+            title: "Property creation",
+            message: "Property is successfully created",
+            variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+    }
+
+    displayError(error) {
+        const toastEvent = new ShowToastEvent({
+            title: "Property is not created",
+            message: error.getMessage(),
+            variant: "destructive"
+        });
+        this.dispatchEvent(toastEvent);
+    }
+    
+}
--- force-app/main/default/lwc/propertyCreation/propertyCreation.js
+++ force-app/main/default/lwc/propertyCreation/propertyCreation.js
@@ -5,6 +5,7 @@ export default class PropertyCreation extends LightningElement {
     checkedRecordTypeIds=[];
     isChoseRecordType = false;
     objectName = 'Property__c';
+    recordTypeId;
 
 
     getRecordTypes(event){
@@ -13,14 +14,16 @@ export default class PropertyCreation extends LightningElement {
 
     submitRecordType(event){
         if (this.checkedRecordTypeIds.length == 1) {
-            //TODO /save record type ID
+            this.recordTypeId = this.checkedRecordTypeIds.checkedValue
             this.isChoseRecordType = true;
         } else {
             alert('Choose one record type!!');
         }
     }
 
-
+    handleRecordCreation(event) {
+        this.isChoseRecordType = false;
+    }
 
 
 }
