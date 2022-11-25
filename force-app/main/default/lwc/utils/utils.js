7565065760a4d0b88ea8aa4dda4bb98cc7d755ac
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-11-26 02:06:30 +0300

    task 2 HW FE 5

--- force-app/main/default/lwc/utils/utils.js
+++ force-app/main/default/lwc/utils/utils.js
@@ -2,7 +2,11 @@ import { NavigationMixin } from 'lightning/navigation';
 
 import PROPERTY_NAME from '@salesforce/schema/Property__c.Name';
 import PROPERTY_SELLING_PRICE from '@salesforce/schema/Property__c.Selling_price__c';
+import PROPERTY_RENT_PRICE from '@salesforce/schema/Property__c.Rent_price__c';
 import PROPERTY_IMAGE_URL from '@salesforce/schema/Property__c.imageURL__c';
+import PROPERTY_COUNTRY from '@salesforce/schema/Property__c.Country__c';
+import PROPERTY_CITY from '@salesforce/schema/Property__c.City__c';
+import PROPERTY_ADDRESS from '@salesforce/schema/Property__c.Address__c';
 import PROPERTY_OWNER from '@salesforce/schema/Property__c.Contact__c';
 import PROPERTY_OWNER_ID from '@salesforce/schema/Property__c.Contact__r.Id';
 import PROPERTY_OWNER_FIRST_NAME from '@salesforce/schema/Property__c.Contact__r.FirstName';
@@ -44,7 +48,11 @@ const navigateTo = (page, type, recordId, objectApiName, actionName) => {
 export{
     PROPERTY_NAME,
     PROPERTY_SELLING_PRICE,
+    PROPERTY_RENT_PRICE,
     PROPERTY_IMAGE_URL,
+    PROPERTY_COUNTRY,
+    PROPERTY_CITY,
+    PROPERTY_ADDRESS,
     PROPERTY_OWNER,
     PROPERTY_OWNER_ID,
     PROPERTY_OWNER_FIRST_NAME,
